84bdc52bc6d8ce9b6652322f0d996dbfa91d84a9
Kay <hello@kunalpanchal.in>
2016-11-26 18:56:08 +0530

    Version 1 Changes.

--- app/src/main/java/com/kay/amirooted/Home.java
+++ app/src/main/java/com/kay/amirooted/Home.java
@@ -3,17 +3,16 @@ package com.kay.amirooted;
 import android.app.ActivityManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.Spanned;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-import static android.R.attr.width;
+import com.intrusoft.library.FrissonView;
+import com.kay.amirooted.Utils.RootUtilities;
 
 public class Home extends AppCompatActivity {
 
@@ -21,52 +20,50 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        final FrissonView frissonView = (FrissonView) findViewById(R.id.frisson_view);
         final TextView deviceInfo = (TextView) findViewById(R.id.device_info);
-        toolbar.setTitle("");
-        setSupportActionBar(toolbar);
+        final TextView rootedStatus = (TextView) findViewById(R.id.tv_root_status);
+
         deviceInfo.setText(updateUI());
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        rootedStatus.setText(RootUtilities.isDeviceRooted() ? "Rooted" : "Not Rooted");
+//        frissonView.setImageSource(R.mipmap.abstractbg);
+        frissonView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                setTitle("kidding");
-                toolbar.setTitle("kidding");
+                Snackbar.make(view, "Refreshed", Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+                deviceInfo.setText(updateUI());
+                rootedStatus.setText(RootUtilities.isDeviceRooted() ? "Rooted" : "Not Rooted");
             }
         });
     }
 
-    public String updateUI() {
-
+    public Spanned updateUI() {
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        int width = metrics.heightPixels;
-        int height = metrics.widthPixels;
-
-
         ActivityManager actManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
         ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
         actManager.getMemoryInfo(memInfo);
         long totalMemory = memInfo.totalMem;
 
-
-
         String str =
-                "Manufacturer : " + Build.MANUFACTURER +
-                        "\nModel : " + android.os.Build.MODEL +
-                        "\nBrand: " + Build.BRAND +
-                        "\nDisplay :" + Build.DISPLAY +
-                        "\nRAM :" + (totalMemory/1048576)+"mb ("+totalMemory+" Bytes)"+
-                        "\nFingerprint : " + Build.FINGERPRINT +
-                        "\nDevice : " + Build.DEVICE +
-                        "\nBoard : " + Build.BOARD +
-                        "\nBootloader : " + Build.BOOTLOADER +
-                        "\nId : " + Build.ID +
-                        "\nProduct :" + Build.PRODUCT +
-                        "\nType : " + Build.TYPE +
-                        "\nReselution : " + width + height;
-        return str;
+                "<b>Manufacturer</b> : " + Build.MANUFACTURER +
+                        "<br><b>Model</b> : " + android.os.Build.MODEL +
+                        "<br><b>Brand</b> : " + Build.BRAND +
+                        "<br><b>Display</b> :" + Build.DISPLAY +
+                        "<br><b>RAM</b> :" + (totalMemory / 1048576) + "mb (" + totalMemory + " Bytes)" +
+                        "<br><b>Fingerprint</b> : " + Build.FINGERPRINT +
+                        "<br><b>Device</b> : " + Build.DEVICE +
+                        "<br><b>Board</b> : " + Build.BOARD +
+                        "<br><b>Bootloader</b> : " + Build.BOOTLOADER +
+                        "<br><b>Id</b> : " + Build.ID +
+                        "<br><b>Product</b> :" + Build.PRODUCT +
+                        "<br><b>Type</b> : " + Build.TYPE +
+                        "<br><b>Resolution</b> : <i>" + metrics.widthPixels + "</i> x <i>" + metrics.heightPixels + "</i>";
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N)
+            return Html.fromHtml(str, Html.FROM_HTML_MODE_LEGACY);
+        else
+            return Html.fromHtml(str);
     }
 }
